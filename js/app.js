7ffd369683356081d81793d0c24d92d2eb4b695f
abhilashlegend <abhilash456a@live.com>
2023-01-24 20:25:44 +0530

    Added Automobile Blog Page

--- js/app.js
+++ js/app.js
@@ -17,3 +17,40 @@
         }, false)
       })
   })()
+
+  /* Function to trim content */
+  const truncateContent = (content, len) => {
+    return content.substr(0, len - 1) + (content.length > len ? '&hellip;' : '');
+  }
+
+  /* Function that displays Blog in Blog Index Page */
+  const displayBlog = (blogdata) => {
+    const blogContainer = document.getElementById("blog");
+
+    let output = '';
+   
+    blogdata.forEach((blog) => {
+        output += `
+        <div class="col-lg-4 col-md-6 mt-5">
+            <div class="card">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+            <img src="${blog.postImage}" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">${blog.title}</h5>
+            <p class="card-text">
+                ${truncateContent(blog.content, 200)}
+            </p>
+            <a href="#!" class="btn primary-btn">Read</a>
+            </div>
+        </div>
+      </div>
+        `;  
+    })
+   
+    blogContainer.innerHTML = output;
+
+}
---
+++ js/automobile.js
@@ -0,0 +1,81 @@
+const automobileBlogData = [
+    {
+        title: "Honda, TVS, Royal Enfield increase market share in April-December 2022",
+        postImage: "./images/automobile/20230124024120_Collage_24_01_23.jpg",
+        content: `
+        <p>SIAM (Society Of Indian Automobile Manufacturers) has released numbers for the period of April-December 2022. The first nine months of the financial year saw sales of 79,39,498 motorcycles (up 17 percent), 39,85,312 scooters (up 32 percent), and 3,32,684 mopeds (down 9 percent). However, these numbers are well below those of April-December 2019 (total sales: 1.39 crore units – scooters: 44,63,954, motorcycles: 89,55,215, mopeds: 4,95,805) as well as April-December 2018 (total sales: 1.65 crore units – scooters: 53,24,125, motorcycles: 1.05 crore, mopeds: 661,923). Let’s examine how each of the major players in the market performed.</p>
+        <h3>Hero MotoCorp: 39,21,309 units</h3>
+        <p>Hero MotoCorp, the country’s two-wheeler market leader, reported sales of 3.92 million units, up 11 percent on year-ago numbers. Of this, 36,39,140 units or 93 percent comprised motorcycles (up 10 percent) and scooters at 2,82,169 units (7 percent) made up the rest.</p>
+        <h3>HMSI: 33,22,746 units</h3>
+        <p>Nearly 6,00,000 units behind the market leader is Honda Motorcycle & Scooter India (HMSI). But compared to Hero, Honda has recorded a higher growth rate, which has translated into a market share increase of two percent – from 25.13 percent a year ago to 27.10 percent.</p>
+        <p>HMSI sold a total of 14,28,996 bikes, up 31 percent, which gives it a market share of 18 percent versus 16 percent a year ago. However, HMSI’s strength comes from its sheer dominance in the scooter market driven by its Activa range, which recently saw the addition of the Activa H-Smart. The company has recorded sales of nearly 1.9 million scooters in the past nine months, growing 29 percent YoY and has a scooter market share of 47.51 down one percent from 48.65 percent a year ago. Put that down to a sluggish market as well as competition from the No. 3 player, TVS.  </p>
+        `,
+        userId: 1,
+        published: "Jan 24, 2023 02:41:00 PM"
+    },
+    {
+        title: "Maruti Suzuki Fronx Sigma base model: in pictures",
+        postImage: "./images/automobile/20230124015407_Fronx_side_resized.jpg",
+        content: `
+        <p>At Auto Expo 2023, Maruti Suzuki unveiled the Baleno-based Fronx SUV along with the much-awaited 5-door Jimny. The bookings for the Fronx are already underway and the SUV will go on sale in April this year. </p>
+        <ol>
+            <li>Fronx Sigma only available in 1.2 MT</li>
+            <li>Deliveries to start in June</li>
+            <li>Starting price to undercut Brezza</li>
+        </ol>
+        <p>The Fronx is available in five trims – Sigma, Delta, Delta+, Zeta and Alpha. It comes with two powertrain options, the 90hp, 1.2-litre petrol engine and the 100hp, 1.0-litre turbo-petrol Boosterjet unit, both of which are offered with Maruti Suzuki’s mild-hybrid technology.</p>
+        <h3>Maruti Suzuki Fronx Sigma: exterior, interior, features </h3>
+        <p>As can be seen from the images, the Fronx Sigma gets steel wheels and black door handles and wing mirrors.  Wheel covers are a part of the equipment list, however,  the model here is not fitted with them.</p>
+        <p>On the inside, the Fronx Sigma gets a dual-tone, black and blue theme and essentials like power windows, tilt adjustment for the steering wheel, and even automatic climate control. However, there is no factory-fitted stereo and the steering wheel lacks controls. </p>
+        <p>Other equipment on offer includes 60:40 split folding rear seats, keyless entry and go, and fabric seat upholstery. Safety is taken care of with rear parking sensors, hill-hold assist, dual airbags and ESP. It also comes with a rear defogger but no rear wash-wipe.</p>
+        <p>Features like a touchscreen with Apple CarPlay and Android Auto connectivity, steering-mounted controls, voice assistant and electrically adjustable wing mirrors are available from the Delta variant onwards. </p>
+        `,
+        userId: 2,
+        published: "Jan 24, 2023 03:13:00 PM"
+    },
+    {
+        title: "Toyota Hyryder waiting period extends up to 15 months",
+        postImage: "./images/automobile/20230124010844_Hyryder 2.jpg",
+        content: `<p>The Maruti Suzuki Grand Vitara and Toyota Hyryder SUVs have had a fair start in the market since their launch last year, selling a combined average of 9,000 units per month. Our dealer sources tell us that while the Hyryder's mild-hybrid variants command a maximum waiting period of up to 15 months, strong-hybrid variants see between 6-7 months.</p>
+        <ul>
+            <li>Longer waiting period for Hyryder's mild hybrid line-up</li>
+            <li>Strong-hybrid variants have a waiting period between 6-7 months</li>
+            <li>Comparatively lower waiting period for the Grand Vitara</li>
+        </ul>
+        <h3>Toyota Hyryder mild hybrid sees a longer waiting period</h3>
+        <p>Toyota offers the Hyryder in four trims – E, S, G and V. As per our dealer sources, when it comes to mild hybrids, the G manual variant has a 15-month waiting period, whereas the automatic sees only 2 months. The top-spec V variant, on the other hand, has a 10-month waiting for the manual and 2 months for the automatic. The AWD version, available only on the V trim with the mild-hybrid MT powertrain, has a waiting period of 3-4 months.</p>
+        <p>However, Toyota has been producing very few units (or almost none) of the base-spec Hyryder. Waiting periods for the base E and S trims on the mild hybrid line-up are currently more than 24 months, making it next to impossible to get your hands on one. In fact, sources tell us that Toyota dealers are trying to push customers more towards the higher-spec trims.</p>
+        <h3>Toyota Hyryder: strong hybrid's waiting period details</h3>
+        <p>The Hyryder's strong-hybrid variants are more readily available across the range. The longest a customer will have to wait for the strong hybrid is 6 to 7 months for the G trim. The top-spec V trim has an even lower waiting period of 3 to 4 months. This is because Toyota has prioritised sales of the strong-hybrid variants as was the production plan from the start, according to people in the know.</p>
+        `,
+        userId: 1,
+        published: "Jan 24, 2023 01:14:00 PM"
+    },
+    {
+        title: "Honda Activa H-Smart launched at Rs 80,537",
+        postImage: "./images/automobile/20230123014428_Activa.jpg",
+        content: `
+        <p>Honda has launched the Activa H-Smart in India at Rs 80,537. It gets a host of keyless features but remains mechanically identical to the standard Activa. The Activa H-Smart is available in six colours. Alongside the Activa H-Smart, Honda also revealed the updated prices for the Activa 6G Standard (Rs 74,536) and the Activa 6G DLX (Rs 77,036).</p>
+        <ol>
+            <li>Activa H-Smart gets keyless fob, electronic features</li>
+            <li>Activa 6G standard, DLX prices up by Rs 1,177</li>
+        </ol>
+        <h3>Honda Activa H-Smart: what's new?</h3>
+        <p>The Honda Activa H-Smart gets an electronic key fob and brings in a bunch of electronic features, some of which are similar to that of a car. The SmartFind feature allows you to locate your vehicle by flashing all four indicators at the press of a button. The SmartStart feature allows you to start the scooter by just flipping the switch located where the key slot would be found normally.</p>
+        <p>The SmartUnlock feature allows you to unlock the handlebar, the fuel filler cap and the underseat storage unit with the use of the key fob. The SmartSafe feature shuts off the keyless functionality of the fob, thereby locking the vehicle and aiding safety. Another feature separating the Activa H-Smart from the regular Activa is the presence of alloy wheels, compared to the steel units found on the Activa 6G. This marks the first time in its nearly 20-year model run that the 110cc Activa gets alloy wheels. Honda has revealed that production of the Activa H-smart will begin by the end of this week and that units will be dispatched to dealers by the end of this month. </p>
+        <h3>Honda Activa H-Smart: pricing</h3>
+        <p>Apart from these features, the Activa H-Smart remains identical to the standard Activa. Both standard variant and the Activa 6G DLX have received a hike of Rs 1,177, now priced at Rs 74,536 and Rs 77,036, respectively. The most direct rival to the Activa H-Smart is the TVS Jupiter but in terms of price it is positioned nearly on par with the drum variant of the TVS Ntorq. </p>
+        <p>All prices mentioned are ex-showroom, Delhi.</p>
+        `,
+        userId: 1,
+        published: "Jan 23, 2023 01:44:00 PM"
+    }
+];
+
+window.localStorage.setItem("autoblog", JSON.stringify(automobileBlogData));
+
+const getAutoBlog = () => autoblog = JSON.parse(window.localStorage.getItem("autoblog"));
+
+const autoBlogData = getAutoBlog();
+
+displayBlog(autoBlogData);
