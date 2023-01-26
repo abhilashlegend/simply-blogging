7ebeedbb4d8d73e8235a30b559b7aa51315cc2f6
abhilashlegend <abhilash456a@live.com>
2023-01-26 12:30:12 +0530

    Finance blog page done

---
+++ js/finance.js
@@ -0,0 +1,72 @@
+const financeBlogData = [
+    {
+        title: "This wealth manager feels fiscal deficit for FY23 may be lower than government's own estimate",
+        postImage: "./images/finance/high-angle-budget-wooden-blocks.jpg",
+        content: `
+        <p>The tax collection during the last nine months has been quite robust and much ahead of the budget estimates for the current year. Hence, there is a good possibility that the government may have a fiscal deficit number which is lower than the budgeted and that could be a positive surprise, Raghvendra Nath of Ladderup Wealth Management told in an interview.</p>
+        <p>The managing director feels that if the budget signals higher growth, estimates for the next year are in line with expectations and there is no significant hike in taxes, the market should give a thumbs up and welcome Budget announcements.</p>
+        <p>Raghvendra, who has over 27 years of corporate experience and a deep knowledge of the financial markets, believes while there is a good probability that 2023 may also see good returns, the returns shall be more stock specific and not so much at a broader market level. Edited excerpts:</p>
+        <h3>Do you think Budget 2023 will focus on populist measures ahead of state elections or it will be growth oriented?</h3>
+        <p>Over the years we have seen that most central governments maintain a good balance between social and economic objectives during the budget. So I don’t expect this year will be any different. The budget should largely be growth oriented to further the momentum of economic recovery.</p>
+        <p>In the last two budgets, we have seen that the government is actively listening to the industry experts and addressing the gaps in policy and taxation. This budget should ideally further that exercise in bringing industry friendly policies that can bolster overall Economic growth.</p>
+        `,
+        userId: 1,
+        published: "Jan 26, 2023 02:41:00 PM"
+    },
+    {
+        title: "Further drop likely if Nifty falls below 17,840 | Bet on these 3 stocks for decent returns in short term",
+        postImage: "./images/finance/19333414.jpg",
+        content: `
+        <p>Bears remained at the helm as the benchmark Nifty 50 index shed 226.35 points (1.25 percent) on January 25. On the daily timeframe, the index remained below the 50-day exponential moving average, which confirms the bearish trend.</p>
+        <p>On the lower end, however, the drop was limited to the upper band of the falling wedge pattern on the daily chart. Sentiment looks very weak, with the RSI (relative strength index) in a bearish crossover.</p>
+        <p>The Nifty 50 ended at 17,891.95 on January 25. A further drop may occur if the index falls below 17,840. On the higher end, resistance is placed at 18,000.</p>
+        <p>The Nifty Bank index witnessed selling pressure throughout the session and ended at 41,647.65. On the daily chart, an upward consolidation was followed by a sharp fall, suggesting a rise in bearish bets in the space.</p>
+        <h3>Here are two buy calls and one sell call for the short term:</h3>
+        <h3>TVS Motor: Buy | LTP: Rs 1,038 | Stop-Loss: Rs 997 | Target: Rs 1,130 | Return: 9 percent</h3>
+        <p>The stock has risen following a consolidation on the daily chart, confirming the increasing bullishness. Besides, the stock price has moved above the 50-day exponential moving average on the daily timeframe.</p>
+        <p>The daily RSI is in positive divergence. In the short term, the stock may rise to Rs 1,130. On the lower end, support is pegged at Rs 997.</p>
+        `,
+        userId: 2,
+        published: "Jan 26, 2023 03:13:00 PM"
+    },
+    {
+        title: "Smartphone shipments plunge most ever as buyers spend less",
+        postImage: "./images/finance/close-up-hand-holding-smartphone.jpg",
+        content: `
+        <p>Shipments declined 18.3% in the December quarter compared to a year earlier, to a little over 300 million units, Needham, Massachusetts-based IDC said Thursday. For the year, shipments fell 11.3% and marked the lowest total for a decade, the researchers said.</p>
+        <p>Global smartphone shipments suffered their worst quarterly drop on record in a clear sign of cooling consumer demand that signals more pain for manufacturing hubs like South Korea and Vietnam.</p>
+        <p>Shipments declined 18.3% in the December quarter compared to a year earlier, to a little over 300 million units, Needham, Massachusetts-based IDC said Thursday. For the year, shipments fell 11.3% and marked the lowest total for a decade, the researchers said.</p>
+        <p>“We have never seen shipments in the holiday quarter come in lower,” Nabila Popal, research director at IDC, said in a press release. Along with inflation and economic uncertainties, Covid lockdowns in China were another factor that hurt the industry, including sales of Apple Inc.’s iPhone, she said. “Heavy sales and promotions during the quarter helped deplete existing inventory rather than drive shipment growth.”</p>
+        <p>Turmoil in Apple’s main Chinese production base may have disrupted shipments in the quarter. Protests over Covid restrictions and living conditions at the Zhengzhou complex that makes the majority of the world’s iPhones derailed production for weeks, culminating in violent protests in December.</p>
+        <p>Smartphones are among the largest exports for Korea and a key source of income for Vietnam as Samsung Electronics Co. operates factories in both countries. Samsung last quarter reported its biggest profit fall in over a decade, primarily led by a drop in demand for semiconductors. The company’s exposure to smartphone sales is amplified by its role as the leading provider of memory and displays for the industry.</p>
+        <p>Mobile gadget sales serve as a barometer of demand for chips. Korea’s smartphone shipments are likely to eke out just 0.7% growth this year after falling 2.1% last year, according to a Korea Development Bank forecast. Chip exports from the country will probably contract 9.8% from a year earlier in 2023, it said, adding to earlier suggestions that a recovery in demand shouldn’t be expected until the tail end of the year.</p>
+        `,
+        userId: 1,
+        published: "Jan 26, 2023 01:14:00 PM"
+    },
+    {
+        title: "Shares in Asia hit fresh 7-month high, US GDP data awaited",
+        postImage: "./images/finance/6256878.jpg",
+        content: `
+        <p>MSCI's broadest index of Asia-Pacific shares outside Japan climbed 0.9% to 557.65 and was set for its fifth straight day of gains.</p>
+        <p>Asian equities rose to a fresh seven-month high on Thursday, with Hong Kong shares playing catch-up to other markets' gains as trade resumed after its three-day Lunar New Holiday.</p>
+        <p>MSCI's broadest index of Asia-Pacific shares outside Japan climbed 0.9% to 557.65 and was set for its fifth straight day of gains.</p>
+        <p>The index has gained 10% so far in January, buoyed by expectations of a strong economic rebound in China and by hopes that most major central banks are nearing an end to hefty rate rises.</p>
+        <p>Trading was thin on Thursday with Australia closed for a holiday and certain parts of Asia, including China, still away for the Lunar New Year.</p>
+        <p>The buoyant mood looked set to continue in Europe, with the Eurostoxx 50 futures up 0.58%, German DAX futures 0.58% higher and FTSE futures up 0.30%.</p>
+        <p>Traders betting that the U.S. Federal Reserve will soon tone down its aggressive rate hike policy got a lift after the Bank of Canada on Wednesday raised rates but became the first major central bank to say it would likely hold off on further increases for now.</p>
+        <p>After a series of super-sized rate hikes last year, the U.S. central bank is now largely expected to raise rates by a smaller 25 basis points next week on signs that inflation is cooling.</p>
+        <p>While analysts expect the Fed to eventually pause its interest rate hikes this year, for some the meeting in February is a bit too early for that.</p>
+        `,
+        userId: 1,
+        published: "Jan 23, 2023 01:44:00 PM"
+    }
+];
+
+window.localStorage.setItem("finblog", JSON.stringify(financeBlogData));
+
+const getFinanceBlog = () => finblog = JSON.parse(window.localStorage.getItem("finblog"));
+
+const finBlogData = getFinanceBlog();
+
+displayBlog(finBlogData);
